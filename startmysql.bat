56f34f309e1df7dc001aa2ba62f22025ec5eeca4
luigi <louis.1995.11.04@gmail.com>
2019-05-11 23:07:51 +0200

    Beep

--- startmysql.bat
+++ startmysql.bat
@@ -1,6 +1,5 @@
 @echo off
-cd ..
 docker stop testsql
 docker rm testsql
-docker build -t mqtt MQTTServerTest
-docker run -d -p 3306:3306 --name testsql -e MYSQL_ROOT_PASSWORD=pass mqtt
+docker build -t mqtt ../MQTTServerTest
+docker run -p 3306:3306 --name testsql -e MYSQL_ROOT_PASSWORD=pass mqtt
